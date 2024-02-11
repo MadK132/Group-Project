2991caec7f278e2de875a3f75c6c3aa39ef85df5
MadK132 <153474301+MadK132@users.noreply.github.com>
2024-02-11 21:09:01 +0600

    Add files via upload

---
+++ src/main/java/org/example/entities/BoardingPass.java
@@ -0,0 +1,34 @@
+package org.example.entities;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.example.FlightSystems;
+import org.example.database.interfaces.IDB;
+import org.example.database.interfaces.PostgresDB;
+import java.sql.SQLException;
+
+@Getter @Setter
+@NoArgsConstructor
+
+public class BoardingPass {
+    private static int id_gen = 1;
+    private int reserve_id;
+    private String phone_number;
+    private String flight_number;
+    IDB db = new PostgresDB();
+
+    public BoardingPass(String phone_number, String flight_number){
+        setReserve_id();
+        setFlight_number(flight_number);
+        setPhone_number(phone_number);
+    }
+
+    public void MakeReservation() throws SQLException {
+        FlightSystems flightSystems = new FlightSystems(db);
+        flightSystems.makeReservation(getFlight_number(), getPhone_number());
+    }
+
+    public void setReserve_id() {
+        this.reserve_id = id_gen++;
+    }
+}
